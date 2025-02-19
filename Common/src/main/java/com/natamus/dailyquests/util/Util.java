a79ae9d955a3cdae35e3a32b389b2c8f27e7a0ac
Rick South <rick@natamus.com>
2025-02-19 11:03:32 +0100

    (v2.1) Fixed a crash on load due to a mod incompatibility. [+1]

--- Common/src/main/java/com/natamus/dailyquests/util/Util.java
+++ Common/src/main/java/com/natamus/dailyquests/util/Util.java
@@ -114,7 +114,7 @@ public class Util {
 					Variables.usableItems.add(itemRegistry.getKey(item));
 				}
 			}
-			catch (NullPointerException ignored) { }
+			catch (Exception ignored) { }
 		}
 
 
@@ -132,7 +132,7 @@ public class Util {
 					Variables.breedableMobs.add(entityTypeRegistry.getKey(entityType));
 				}
 			}
-			catch (NullPointerException ignored) { }
+			catch (Exception ignored) { }
 		}
 
 
